e3b2f82c984bb431df9ba3cfe82323df1dbabcfc
Noam Loewenstern <noamloewenstern@gmail.com>
2023-05-14 23:53:58 +0300

    FIXED checking for VITE_REPO_NAME env var in vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd());
+  if (typeof env.VITE_REPO_NAME === 'undefined') {
+    throw new Error('VITE_REPO_NAME is undefined');
+  }
   return {
-    // base: `/${env.REP}/`,
     base: env.VITE_REPO_NAME,
     plugins: [react()],
   };
